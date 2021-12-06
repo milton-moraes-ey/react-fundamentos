d6fd58d1fa6d00f3892ced448edeca57e1339daa
soaresmilton <eng.milton.soares@gmail.com>
2021-12-06 18:00:27 -0300

    Configurando Loaders

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <h1 id="title">Configurando o WebPack na mao</h1>,
+  <h1 id="title">Configurando os loaders do babel no webpack</h1>,
   document.getElementById('root'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle[hash].js',
@@ -13,5 +13,15 @@ module.exports = {
       template: path.resolve(__dirname, 'public', 'index.html')
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ]
+  }
 };
