08579d400881692dd529e309477ee3c66dde748d
soaresmilton <eng.milton.soares@gmail.com>
2021-12-07 19:23:31 -0300

    Add aula 07: Fragments

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from "react";
 
 
 function App() {
-  return <h1>Componente App</h1>
+  return (
+    <>
+      <h1>Componente App</h1>
+      <h2>Subtitulo do componente</h2>
+      <p>Parágrafo do inserido no componente</p>
+      <section>
+        <h3>Titulo h3 da section</h3>
+        <article>Artigo da section com H3</article>
+      </section>
+    </>
+  );
 }
 
 export default App;
