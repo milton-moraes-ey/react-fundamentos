c3f34433a13122b6c0bd974021198a73d62b4b39
soaresmilton <eng.milton.soares@gmail.com>
2021-12-07 20:28:20 -0300

    Add aula 08: Expressoes JS no JSX

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 
+const category = 'Posts da semana';
+
 
 function App() {
   return (
     <>
-      <h1>Componente App</h1>
-      <h2>Subtitulo do componente</h2>
-      <p>Parágrafo do inserido no componente</p>
-      <section>
-        <h3>Titulo h3 da section</h3>
-        <article>Artigo da section com H3</article>
-      </section>
+      <h1>Meu BLOG</h1>
+      <h2>{category}</h2>
     </>
   );
 }
