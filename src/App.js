cd780517221b219b664d694eadd9d2766d38e972
soaresmilton <eng.milton.soares@gmail.com>
2021-12-09 18:12:52 -0300

    Add aula 09: O que sao Props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Post from "./Post";
 
 const category = 'Posts da semana';
+const titulo01 = 'Titulo do primeiro Post';
 
 
 function App() {
@@ -8,6 +10,14 @@ function App() {
     <>
       <h1>Meu BLOG</h1>
       <h2>{category}</h2>
+
+      <hr />
+
+      <Post title={titulo01} subtitle="Subtítulo da notícia 01"/>
+      <Post title="Título do Post 02" subtitle="Subtítulo da notícia 02"/>
+      <Post title="Título do Post 03" subtitle="Subtítulo da notícia 03"/>
+      <Post title="Título do Post 04" subtitle="Subtítulo da notícia 04"/>
+   
     </>
   );
 }
