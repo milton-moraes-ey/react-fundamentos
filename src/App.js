0e300cfa2b246344413dd4cb8ce2da5383620b9c
soaresmilton <eng.milton.soares@gmail.com>
2021-12-10 16:16:19 -0300

    Aula 10: Props Children

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
+import Header from "./Header";
 import Post from "./Post";
 
-const category = 'Posts da semana';
 const titulo01 = 'Titulo do primeiro Post';
 
 
 function App() {
   return (
     <>
-      <h1>Meu BLOG</h1>
-      <h2>{category}</h2>
+      <Header title="Meu BLOG">
+        <h2>Posts da semana</h2>
+      </Header>
 
       <hr />
 
-      <Post title={titulo01} subtitle="Subtítulo da notícia 01"/>
-      <Post title="Título do Post 02" subtitle="Subtítulo da notícia 02"/>
-      <Post title="Título do Post 03" subtitle="Subtítulo da notícia 03"/>
-      <Post title="Título do Post 04" subtitle="Subtítulo da notícia 04"/>
-   
+      <Post title={titulo01} subtitle="Subtítulo da notícia 01" />
+      <Post title="Título do Post 02" subtitle="Subtítulo da notícia 02" />
+      <Post title="Título do Post 03" subtitle="Subtítulo da notícia 03" />
+      <Post title="Título do Post 04" subtitle="Subtítulo da notícia 04" />
+
     </>
   );
 }
