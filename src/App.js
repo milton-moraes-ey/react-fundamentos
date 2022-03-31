81b580cd553430777182a0008ff976cf38ca61d2
Milton A. Soares Moraes <milton.moraes@br.ey.com>
2022-03-31 19:07:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Post from "./Post";
 
-const titulo01 = "Titulo do primeiro Post";
-
 function App() {
-  const posts = [
+  const [posts, setPosts] = useState([
     {
+      id: Math.random(),
       title: "Title#01",
       subtitle: "Sub#01",
       likes: 20,
     },
     {
+      id: Math.random(),
       title: "Title#02",
       subtitle: "Sub#02",
       likes: 12,
     },
     {
+      id: Math.random(),
       title: "Title#03",
       subtitle: "Sub#03",
       likes: 5,
     },
-  ];
+  ]);
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Sub#0${prevState.length + 1}`,
+          likes: 40,
+        },
+      ]);
+    }, 500);
+  }
 
   return (
     <>
       <Header>
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
 
       {posts.map((post) => (
         <Post
-          key={post.title}
+          key={post.id}
           likes={post.likes}
           post={{
             title: post.title,
